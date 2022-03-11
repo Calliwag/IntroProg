70befc414e0cc7e32fb12a6a7adcbed173f3c246
Calvin Akom <calvinakom@gmail.com>
2022-03-11 10:48:17 -0500

    Initial Commit

--- Yahtzee/main.cpp
+++ Yahtzee/main.cpp
@@ -14,21 +14,58 @@ void displayDice(vector<int> dice)
     cout << endl;
 }
 
-void rollDice(vector<int>& dice)
+void roll(vector<int>& dice, vector<bool> reroll = {true,true,true,true,true})
 {
     for(int i = 0; i < dice.size(); i++)
     {
-        dice[i] = rand()%6 + 1;
+        if(reroll[i] == true)
+        {
+            dice[i] = rand()%6 + 1;
+        }
     }
+}
+
+void reroller(vector<int>& dice)
+{
+    for(int i = 0; i < 2; i++)
+    {
+        vector<bool> reroll{false,false,false,false,false};
+        bool toRoll;
+        cout << "Would you like to reroll (type 1 for yes or 0 for no)?" << endl;
+        cin >> toRoll;
+        if(toRoll == false)
+        {
+            break;
+        }
+        cout << "What dice would you like to reroll or hold (type 0 for hold and 1 for reroll)?" << endl;
+        for(int it = 0; it < 5; it++)
+        {
+            int current;
+            cin >> current;
+            if(current == 1)
+            {
+                reroll[it] = true;
+            }
+        }
+        roll(dice, reroll);
+        displayDice(dice);
+    }
+}
+
+void numScore(vector<int>& dice, int num)
+{
+
 }
 
 int main()
 {
-    srand(time(NULL));
-
+    srand(time(0));
     vector<int> dice{1,2,3,4,5};
-    rollDice(dice);
+    roll(dice);
     displayDice(dice);
+    //reroller(dice);
+
+
 
     return 0;
 }
