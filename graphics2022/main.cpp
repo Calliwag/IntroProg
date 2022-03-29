44df14f8cab6ce597377d03c2500f54bbd2cc7e3
Calvin Akom <calvinakom@gmail.com>
2022-03-29 10:52:39 -0400

    Graphics

---
+++ graphics2022/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "graphics.h"
+
+using namespace std;
+using namespace mssm;
+
+#pragma GCC diagnostic ignored "-Wsign-compare"
+
+int main()
+{
+    Graphics g("Test", 1024, 768);
+
+    while (g.draw()) {
+
+        g.line({10,10}, {100,200}, GREEN);
+        g.text({200,200}, 20, "Hello");
+
+        if (g.isKeyPressed(Key::ESC)) {
+            break;
+        }
+
+        if (g.isKeyPressed(Key::Left)) {
+            // g.cout << "Left Key Down" << endl;
+
+        }
+
+        for (const Event& e : g.events()) {
+            g.cerr << e << endl;
+
+            switch (e.evtType) {
+            case EvtType::KeyPress:
+                break;
+            case EvtType::KeyRelease:
+                break;
+            case EvtType::MouseMove:
+                break;
+            case EvtType::MousePress:
+                break;
+            case EvtType::MouseRelease:
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
