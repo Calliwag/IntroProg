8c1679cf2361ba5cc98fa107f800c0d9db3f04ea
Calvin Akom <calvinakom@gmail.com>
2022-04-04 10:37:38 -0400

    Pong2

--- pongCalvin/main.cpp
+++ pongCalvin/main.cpp
@@ -13,14 +13,14 @@ int main()
     Graphics g("Test", 400, 400);
     Vec2d player{375,200};
     Vec2d bot{25,200};
-    double speed = 1.5;
-    double botspeed = 1.2;
+    double speed = 1.7;
+    double botspeed = 1.4;
     Vec2d ballpos{200,200};
     Vec2d ballvel{0,0};
     int linel = 15;
     ballvel.x = 4;
     ballvel.y = (rand()%10)/10.0;
-    int mult = rand()%1;
+    int mult = rand()%2;
     if(mult == 0)
     {
         ballvel.y -= 1;
@@ -34,7 +34,9 @@ int main()
     int freezecount = 0;
     while (g.draw())
     {
-        g.cout << setw(3) << botscore <<" : "<< setw(3) << playscore;
+        g.cout << setw(33) << botscore <<"   : "<< setw(3) << playscore;
+        //double test = (playscore - botscore)/10;
+        //ballvel.x = 4 + test;
         int playertop = player.y+linel;
         int playerbot = player.y-linel;
         int bottop = bot.y+linel;
@@ -42,19 +44,19 @@ int main()
         g.line({player.x, playertop},{player.x, playerbot});
         g.line({bot.x, bottop},{bot.x, botbot});
         g.ellipse(ballpos, 3, 3);
-        if(g.isKeyPressed(Key::Up))
+        if(g.isKeyPressed(Key::Up) && !(playertop <= 0))
         {
             player.y -= speed;
         }
-        if(g.isKeyPressed(Key::Down))
+        if(g.isKeyPressed(Key::Down) && !(playerbot >= 400))
         {
             player.y += speed;
         }
-        if(ballpos.y > botbot)
+        if(ballpos.y > bot.y)
         {
             bot.y += botspeed;
         }
-        if(ballpos.y < bottop)
+        if(ballpos.y < bot.y)
         {
             bot.y -= botspeed;
         }
@@ -65,7 +67,17 @@ int main()
             ballpos = {200,200};
             player.y = 200;
             bot.y = 200;
-            ballvel.y = (rand()%20)/10.0 - 1;
+            ballvel.y = (rand()%10)/10.0;
+            int mult = rand()%2;
+            if(mult == 0)
+            {
+                ballvel.y -= 1;
+            }
+            if(mult == 1)
+            {
+                ballvel.y += 1;
+            }
+            freezecount = 0;
         }
         if(ballpos.x >= 397)
         {
@@ -73,7 +85,17 @@ int main()
             ballpos = {200,200};
             player.y = 200;
             bot.y = 200;
-            ballvel.y = (rand()%20)/10.0 - 1;
+            ballvel.y = (rand()%10)/10.0;
+            int mult = rand()%2;
+            if(mult == 0)
+            {
+                ballvel.y -= 1;
+            }
+            if(mult == 1)
+            {
+                ballvel.y += 1;
+            }
+            freezecount = 0;
         }
         if(ballpos.y <= 3)
         {
@@ -91,13 +113,14 @@ int main()
         {
             ballvel.x *= -1;
         }
-        if(freezecount >30)
+        if(freezecount >75)
         {
             ballpos += ballvel;
         }
         else
         {
             freezecount ++;
+            g.line(ballpos,ballpos+5*ballvel);
         }
 
     }
