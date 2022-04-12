e580414b8eef9306941ec0af3bdd9c2adc75fe75
Calvin Akom <calvinakom@gmail.com>
2022-04-12 10:53:19 -0400

    Connect 4

--- Connect4/main.cpp
+++ Connect4/main.cpp
@@ -7,9 +7,8 @@ using namespace mssm;
 
 #pragma GCC diagnostic ignored "-Wsign-compare"
 
-Graphics g("Test", 1600, 900);
 
-void drawTable(vector<vector<int>> board)
+void drawTable(vector<vector<int>> board, Graphics& g)
 {
 
     for(int iy = 0; iy < 6; iy++)
@@ -32,6 +31,10 @@ void drawTable(vector<vector<int>> board)
 int main()
 {
 
+    int x = 800;
+    int y = 800;
+    Graphics g("Test", x, y);
+
     vector<vector<int>> board;
     board.resize(7);
 
@@ -48,24 +51,86 @@ int main()
         }
     }
 
+    Vec2d current{x/16,y/16};
+    int currentPlayer = 0;
+
     while (g.draw())
     {
-        for(int iy = 0; iy < 6; iy++)
+        //        for(int iy = 0; iy < 6; iy++)
+        //        {
+        //            for(int ix = 0; ix < 7; ix++)
+        //            {
+        //                if(board[ix][iy] >= 10)
+        //                {
+        //                    g.cout << "   " << board[ix][iy];
+        //                }
+        //                else
+        //                {
+        //                    g.cout << "     " << board[ix][iy];
+        //                }
+        //            }
+        //            g.cout << endl;
+        //        }
+
+        if(currentPlayer == 0)
         {
-            for(int ix = 0; ix < 7; ix++)
-            {
-                if(board[ix][iy] >= 10)
-                {
-                    g.cout << "   " << board[ix][iy];
-                }
-                else
-                {
-                    g.cout << "     " << board[ix][iy];
-                }
-            }
-            g.cout << endl;
+            Color currentColor = YELLOW;
         }
+
+        if(currentPlayer == 1)
+        {
+            Color currentColor = RED;
+        }
+
+        for(int i = 1; i < 8; i++)
+        {
+            g.line({i*x/8, 0},{i*x/8,y});
+            g.line({0,i*y/8},{x,i*y/8});
+        }
+
+        g.ellipse(current, x/8, y/8);
+
+        //        if (g.isKeyPressed(Key::Left) && !(current.x < x/16))
+        //        {
+        //            current.x -= x/8;
+        //        }
+        //        if (g.isKeyPressed(Key::Right) && !(current.x > 15*x/16))
+        //        {
+        //            current.x += x/8;
+        //        }
+
+        for (const Event& e : g.events())
+        {
+            g.cerr << e << endl;
+            switch (e.evtType)
+            {
+            case EvtType::KeyPress:
+
+                if(e.arg == static_cast<int>(Key::Left))
+                {
+                    current.x -= x/8;
+                }
+
+                if(e.arg == static_cast<int>(Key::Right))
+                {
+                    current.x += x/8;
+                }
+
+                break;
+            case EvtType::KeyRelease:
+                break;
+            case EvtType::MouseMove:
+                break;
+            case EvtType::MousePress:
+                break;
+            case EvtType::MouseRelease:
+                break;
+            default:
+                break;
+            }
+        }
+
     }
-    return 0;
+
 }
 
